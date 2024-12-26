448603e34bb0a29c660e578fa8d6261c0a9f9429
suhas-koheda <sharmasuhas450@gmail.com>
2024-12-27 02:29:52 +0530

    Design

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  
+  images: {
+    domains: [
+      'shashank-portfolio-flame.vercel.app'
+    ],
+  },
+
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,64 @@
+import { ArrowLeft, ArrowRight } from "lucide-react";
 import Image from "next/image";
 
 export default function Home() {
+  var data = [
+    ["https://shashank-portfolio-flame.vercel.app/assets/img/twitter-logo.png", "title", "description"],
+    ["image link 2", "title 2", "description 2"]
+  ];
+
   return (
     <>
-    <div className="flex flex-row justify-between">
-      <div>
-        <h1>Check</h1>
+      <h2 className="text-center text-3xl font-black">Title goes here</h2>
+      <div className="flex flex-row justify-around py-16 px-10 xl:px-96">
+        <div className="flex items-center justify-center">
+          <button className="bg-gray-600 text-white rounded-full p-2">
+            <ArrowLeft color="#ffffff" />
+          </button>
+        </div>
+
+      <div className="flex justify-around">
+        <div className="hidden xl:flex flex-col items-center justify-center">
+        <Image
+            src={data[0][0]}
+            width={300}
+            height={300}
+            alt="Picture of the author"
+            className="p-10 "
+          />
+          <h1>{data[1][1]}</h1>
+        </div>
+
+        <div className="xl:flex flex-col items-center justify-center">
+          <Image
+            src={data[0][0]}
+            width={500}
+            height={500}
+            alt="Picture of the author"
+            className="p-10 "
+          />
+          <h1>{data[1][1]}</h1>
+          <h3>{data[1][2]}</h3>
+        </div>
+
+        <div className="hidden xl:flex flex-col items-center justify-center">
+        <Image
+            src={data[0][0]}
+            width={300}
+            height={300}
+            alt="Picture of the author"
+            className="p-10"
+          />
+          <h1>{data[1][1]}</h1>
+        </div>
+        </div>
+        <div className="flex items-center justify-center">
+          <button className="bg-gray-600 text-white rounded-full p-2">
+            <ArrowRight color="#ffffff" />
+          </button>
+        </div>
+
       </div>
-      <div>
-        <h1>Check</h1>
-      </div>
-    </div>
-    </>  );
+    </>
+  );
 }
